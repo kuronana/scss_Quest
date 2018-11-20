b797eee3359bfbf08f28a0adf824bcb543e6f0b1
Jess Vachet <jess94i@hotmail.fr>
2018-11-20 18:19:13 +0100

    Préprocesseur CSS finished

---
+++ precious.php
@@ -0,0 +1,94 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <title>FindThePrecious.com</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body class="container-fluid">
+<header class="row">
+    <nav class="navbar navbar-expand-lg navbar-light">
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <div class="navbar-brand">
+                <img src="intfinal/logo.png" class="d-inline-block align-top" alt="title">
+            </div>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link mx-auto" href="#wanted" style="width: 200px;">Fellows</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link mx-auto" href="#ring" style="width: 200px;">The Ring</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active mx-auto" href="#secreward" style="width: 200px;">Get my reward</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active mx-auto" href="#hunters" style="width: 200px;">Best Hunters</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active mx-auto" href="#army" style="width: 200px;">Join the army</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active mx-auto" href="#contactus" style="width: 200px;">Contact us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<div class="row kingpouet">
+    <div class="ring1"></div>
+    <div>
+        <h1>My precious</h1>
+    </div>
+    <div class="ring1"></div>
+</div>
+
+
+<div class="row kingpouet">
+    <div class="ring2"></div>
+    <div>
+        <h2>The one ring</h2>
+    </div>
+</div>
+
+<div class=" row plop">
+    <div class="ring3-1"></div>
+    <div class="ring3-2"></div>
+    <div class="ring3-3"></div>
+    <div class="ring3-4"></div>
+    <div class="ring3-5"></div>
+    <div class="ring3-6"></div>
+</div>
+<div class="row kingpouet">
+    <div>
+        <h1>Please find it !</h1>
+    </div>
+</div>
+
+<footer id="footer" class="row">
+    <ul class="foot col-3">
+        <li class="frtfoot"><a class="colorfoot" href="#">About us</a></li>
+        <li class="frtfoot"><a class="colorfoot" href="fellows.html">Fellows</a></li>
+        <li class="frtfoot"><a class="colorfoot" href="army.html">Join our army</a></li>
+    </ul>
+    <ul class="foot col-6">
+        <li class="sndfoot"><a class="colorfoot" href="#">FAQ</a></li>
+        <li class="sndfoot"><a class="colorfoot" href="#">Reward conditions</a></li>
+        <li class="sndfoot"><a class="colorfoot" href="#">Legal mentions</a></li>
+    </ul>
+    <ul class="foot col-3">
+        <li class="thrdfoot"><a class="colorfoot" href="https://www.Sauron4Ever.com">Sauron4Ever.com</a></li>
+        <li class="thrdfoot"><a class="colorfoot" href="#">Follow him also on twitter</a></li>
+    </ul>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
